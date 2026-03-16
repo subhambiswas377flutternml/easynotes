7c9efe9c8af18c5d060e613139aacb16c2f1dcd3
Subham Biswas <subhambiswas129@gmail.com>
2026-03-16 23:11:16 +0530

    created 2 screens

--- app/src/main/java/com/app/easynotes/core/AppRouter.kt
+++ app/src/main/java/com/app/easynotes/core/AppRouter.kt
@@ -5,11 +5,15 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.app.easynotes.LandingScreen
+import com.app.easynotes.presentation.walkthrough.WalkThroughA
 import kotlinx.serialization.Serializable
 
 object Routes {
     @Serializable
     data object LandingRoute
+
+    @Serializable
+    data object WalkA
 }
 
 @Composable
@@ -18,8 +22,11 @@ fun App(){
     NavHost(navController = navController, startDestination = Routes.LandingRoute) {
 
         composable<Routes.LandingRoute> {
-            LandingScreen()
+            LandingScreen(navController=navController)
         }
 
+        composable<Routes.WalkA> {
+            WalkThroughA()
+        }
     }
 }
